f34bfe998e388c95c29a52fdcf61d2a20a3dc069
Liene Briede <lienebriede84@gmail.com>
2024-10-02 10:10:19 +0000

    Add routing

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the Home Page!</h1>} />
+        <Route path="/signin" element={<h1>Sign In Page</h1>} />
+        <Route path="/signup" element={<h1>Sign Up Page</h1>} />
+      </Routes>
     </div>
   );
 }
--- my-react-app/src/components/NavBar.js
+++ my-react-app/src/components/NavBar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
-import styles from '../styles/NavBar.module.css'
+import styles from '../styles/NavBar.module.css';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -12,16 +13,10 @@ const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
-            <Nav.Link>
-              <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+          <Nav className="ms-auto text-left">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/signin">Sign In</Nav.Link>
+            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
