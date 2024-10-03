cac427e080f000cee178313cc019ce120d793321
Liene Briede <lienebriede84@gmail.com>
2024-10-03 07:42:03 +0000

    Refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-code-institute-915faa2a0d75.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
